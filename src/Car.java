ded65ae32c8c64b4dde74ebf75140fb097575b3b
Gulkaiyr_Bakytbekova <bakytbekova6869@gmail.com>
2022-11-08 20:11:21 +0600

    first commit

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public Car() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
